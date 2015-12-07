6ebe0a0653059b61c3d04641e6913479b026f185
Nicolas Galler <ngaller@gmail.com>
2015-12-07 08:29:21 -0500

    Addl opengraph data

--- src/templates/social_share.php
+++ src/templates/social_share.php
@@ -1,15 +1,22 @@
 <!doctype html>
-<?php
-/**
- * social_share.php
- * Custom template used for the "Social Share" custom post type.
- * The template renders meta tags appropriate for the social network the share was generated for and updates the crawl
- * flag.
- * If the user agent is not a crawler, then the template instead performs a redirection to the exhibition's URL.
- */
+<html>
+<head>
+    <?php
+    /**
+     * social_share.php
+     * Custom template used for the "Social Share" custom post type.
+     * The template renders meta tags appropriate for the social network the share was generated for and updates the crawl
+     * flag.
+     * If the user agent is not a crawler, then the template instead performs a redirection to the exhibition's URL.
+     */
 
-while (have_posts()) {
-    the_post();
-    $shareData = \AWC\SocialPledge\SocialSharePostType::getSocialMetaData();
-    $shareData->generateMetaTags();
-}
+    while (have_posts()) {
+        the_post();
+        $shareData = \AWC\SocialPledge\SocialSharePostType::getSocialMetaData();
+        $shareData->generateMetaTags();
+    }
+    ?>
+</head>
+<body>
+</body>
+</html>
