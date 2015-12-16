01a4576119bb68053b9568a4f981f28b23dacb10
Nicolas Galler <ngaller@gmail.com>
2015-12-16 10:50:27 -0500

    Cleanup option page (use F1 wp utils)

--- src/OptionPage.php
+++ src/OptionPage.php
@@ -8,86 +8,33 @@
 namespace AWC\SocialPledge;
 
 
-class OptionPage
+use F1\WPUtils\Admin\AdminPageHelper;
+
+class OptionPage extends AdminPageHelper
 {
     const AWC_SOCIALPLEDGE_SETTINGS = 'awc_social_pledge_settings';
     const OPTION_FACEBOOK_APPID = 'facebook_appid';
     const OPTION_TWITTER_SCREENNAME = 'twitter_screenname';
 
+    function __construct()
+    {
+        parent::__construct(self::AWC_SOCIALPLEDGE_SETTINGS, 'AWFC Social Pledge Settings');
+        $this->addSetting(self::OPTION_FACEBOOK_APPID, 'Facebook App Id');
+        $this->addSetting(self::OPTION_TWITTER_SCREENNAME, 'Twitter Screenname');
+    }
+
     public function registerOptionPage()
     {
-        add_action('admin_init', array(&$this, 'adminInit'));
-        add_action('admin_menu', array(&$this, 'adminMenu'));
-        add_filter("plugin_action_links_" . AWC_SOCIAL_PLEDGE_PLUGIN_BASENAME, [&$this, 'settingsLink']);
+        parent::registerOptionPage(AWC_SOCIAL_PLEDGE_PLUGIN_BASENAME);
     }
 
-    public function settingsLink($links)
-    {
-        $settings = '<a href="options-general.php?page=' . self::AWC_SOCIALPLEDGE_SETTINGS . '">Settings</a>';
-        array_unshift($links, $settings);
-        return $links;
-    }
-
-    public function adminInit()
-    {
-        register_setting(self::AWC_SOCIALPLEDGE_SETTINGS, self::AWC_SOCIALPLEDGE_SETTINGS, array(&$this, 'sanitizeOptions'));
-        add_settings_section('default', 'AWC Social Pledge Settings', null, self::AWC_SOCIALPLEDGE_SETTINGS);
-        add_settings_field(self::OPTION_FACEBOOK_APPID, 'Facebook App Id',
-            array(&$this, 'createSettingsTextbox'), self::AWC_SOCIALPLEDGE_SETTINGS,
-            'default', array('name' => self::OPTION_FACEBOOK_APPID));
-        add_settings_field(self::OPTION_TWITTER_SCREENNAME, 'Twitter Screenname',
-            array(&$this, 'createSettingsTextbox'), self::AWC_SOCIALPLEDGE_SETTINGS,
-            'default', array('name' => self::OPTION_TWITTER_SCREENNAME));
-    }
-
-    public function adminMenu()
-    {
-        if (current_user_can('manage_options')) {
-            add_options_page('AWC Social Pledge Settings', 'Social Pledge', 'manage_options',
-                self::AWC_SOCIALPLEDGE_SETTINGS, array(&$this, 'outputPluginSettingsPage'));
-        }
-    }
-
-    public function outputPluginSettingsPage()
-    {
-        /** @noinspection HtmlUnknownTarget */
-        echo '<div class="wrap"><form method="POST" action="options.php">';
-
-        settings_fields(self::AWC_SOCIALPLEDGE_SETTINGS);
-        do_settings_sections(self::AWC_SOCIALPLEDGE_SETTINGS);
-        submit_button();
-
-        echo '</form></div>';
-    }
-
-
-    public function createSettingsTextbox($args)
-    {
-        $optionName = $args['name'];
-        $size = isset($args['size']) ? $args['size'] : 40;
-        $placeholder = isset($args['placeholder']) ? esc_attr($args['placeholder']) : '';
-        $option = $this->getOption($optionName);
-        $value = isset($option) ? esc_attr($option) : '';
-        echo "<input type='text' id='$optionName'
-            name='" . self::AWC_SOCIALPLEDGE_SETTINGS . "[$optionName]'
-            value='$value' size='$size' placeholder='$placeholder'/>";
-    }
-
-    public function sanitizeOptions($options)
+    public function onSanitizeOptions($options)
     {
         if (isset($options[self::OPTION_TWITTER_SCREENNAME])) {
             // remove @ from twitter name if they included it
             $options[self::OPTION_TWITTER_SCREENNAME] = str_replace('@', '', $options[self::OPTION_TWITTER_SCREENNAME]);
         }
-        return $options;
-    }
-
-    public static function getOption($optionName)
-    {
-        $options = get_option(self::AWC_SOCIALPLEDGE_SETTINGS);
-        if (is_array($options) && isset($options[$optionName]))
-            return $options[$optionName];
-        return '';
+        return parent::onSanitizeOptions($options);
     }
 
 }
